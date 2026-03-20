bf910476ed32c1db630c7b95f8dcab68b35cf792
SantoshK0809 <santoshkirtane0809@gmail.com>
2026-03-20 11:11:24 +0530

    Implemented login-register and integrated frontend with backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const authRouter = require("./routes/auth.routes.js");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRouter);
 
